5144f6bfa4b5aea24ba3e972dc34824f3f912fa5
Foxy <77237091+Vufes@users.noreply.github.com>
2021-01-11 21:26:35 +0100

    Added loader of font
    Currently hard-coded, but idea is to make it better.

--- scripts/parse_charmap.py
+++ scripts/parse_charmap.py
@@ -1,2 +1,23 @@
+from PIL import Image
 import numpy as np
-import cv2
+
+# currently it loads image of specific font and save them as sprites
+# next goal is to add dictionary
+def load_oldschool():
+    font = Image.open("../Fonts/charmap-oldschool_black.png")
+    arr = np.array(font)
+
+    sprite_size  = np.array([7,5])
+    offset       = np.array([2,2])
+    start        = np.array([1,1])
+    charmap_size = np.array([6,18])
+
+    sprites = np.zeros((charmap_size[0]*charmap_size[1],\
+                        sprite_size[0],sprite_size[1]))
+
+    for y in range(charmap_size[0]):
+        for x in range(charmap_size[1]):
+            reg = start + np.array([y,x]) * (sprite_size + offset)
+            sprites[y*charmap_size[1]+x] = arr[reg[0]:reg[0]+sprite_size[0],\
+                                               reg[1]:reg[1]+sprite_size[1],0]
+    return sprites
