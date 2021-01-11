1382e8ab4f1d5013e729ea73df568a8f2075035a
Foxy <77237091+Vufes@users.noreply.github.com>
2021-01-11 21:39:48 +0100

    test to create numbers 0-9

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,11 @@
-import parse_charmap
+import numpy as np
+from parse_charmap import *
+
+sp = load_oldschool()
+
+canvas = np.full((7,10*6,3),255,dtype=np.uint8)
+
+for i in range(10):
+    canvas[:,i*6:i*6+5] = sp[16+i]
+im = Image.fromarray(canvas)
+im.save("example.png","png")
