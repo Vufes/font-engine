3270365ab7123e1f8f3085ab3a59252d83f09533
Foxy <77237091+Vufes@users.noreply.github.com>
2021-01-11 23:16:59 +0100

    Function to create canvas from ascii charmaps

--- scripts/text_to_canvas.py
+++ scripts/text_to_canvas.py
@@ -1,3 +1,11 @@
-from parse_charmap import *
+import numpy as np
 
-def text_image():
+# For ascii charmaps
+def text_image(text, sprites, s = 1):
+    arr_s = sprites.shape
+    canvas = np.full((arr_s[1]*s,(arr_s[2]+1)*s*len(text), arr_s[3]),\
+                     255, dtype=np.uint8)
+    for i,lt in enumerate(text):
+        x = (arr_s[2]+1)*s*i
+        canvas[:,x:x+arr_s[2]*s] = sprites[ord(lt)-32]
+    return canvas
